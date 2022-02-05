36bdb2d4f48b078979e4fea17b0b03b6ed2615f2
Neil Armstrong <neil@tritium.space>
2022-02-05 13:46:27 +0100

    feat mobile changes

--- build/lib/index.js
+++
@@ -1,5 +0,0 @@
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-    window.location.replace("m.home.html");
-} else {
-    window.location.replace("home.html");
-}
--- build/lib/main.js
+++ build/lib/main.js
@@ -1,31 +1,35 @@
-setTimeout(function () {
-    document.body.classList.remove("preload");
-}, 300);
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    window.location.replace("mobile/index.html");
+} else {
+    setTimeout(function () {
+        document.body.classList.remove("preload");
+    }, 300);
 
-const initScrollCapture = () => {
-    const HEADER = "header";
-    const LOGO_CONTAINER = "logo-container";
+    const initScrollCapture = () => {
+        const HEADER = "header";
+        const LOGO_CONTAINER = "logo-container";
 
-    const h = document.getElementById(HEADER);
-    const l = document.getElementById(LOGO_CONTAINER);
+        const h = document.getElementById(HEADER);
+        const l = document.getElementById(LOGO_CONTAINER);
 
-    window.onscroll = () => {
-        if (window.pageYOffset > 0) {
-            h.classList.remove("enlarged");
-            h.classList.add("shrunk");
+        window.onscroll = () => {
+            if (window.pageYOffset > 0) {
+                h.classList.remove("enlarged");
+                h.classList.add("shrunk");
 
-            l.classList.remove("enlarged");
-            l.classList.add("shrunk");
-        } else {
-            h.classList.remove("shrunk");
-            h.classList.add("enlarged");
+                l.classList.remove("enlarged");
+                l.classList.add("shrunk");
+            } else {
+                h.classList.remove("shrunk");
+                h.classList.add("enlarged");
 
-            l.classList.remove("shrunk");
-            l.classList.add("enlarged");
+                l.classList.remove("shrunk");
+                l.classList.add("enlarged");
+            }
         }
     }
-}
 
-(function main() {
-    initScrollCapture();
-})();
+    (function main() {
+        initScrollCapture();
+    })();
+}

2dca2e4536eacbb6b3d6e9e71d366c9d0017f36b
Julien Kellerhals <julien.kellerhals@gmail.com>
2022-04-06 12:54:35 +0200

    feat: close menu when clicking outside of nav box

--- build/mobile/lib/main.js
+++ build/mobile/lib/main.js
@@ -4,18 +4,33 @@ const interval = 150
 const mobileNavContainer = document.querySelector("[data-js='mobile-navigation-container']");
 const hamburgerDiv = document.querySelector("[data-js='hamburger']")
 
+const mobileScroller = document.getElementsByClassName("mobile-scroller")[0];
+const scrollerOverlay = document.getElementsByClassName("scroller-overlay")[0]
+
 setTimeout(function () {
     document.body.classList.remove("preload");
 }, 500);
 
 hamburgerDiv.addEventListener("click", () => {
     if (hamburgerDiv.dataset.status == "open") {
-        hamburgerDiv.dataset.status = "closed";
-        hamburgerDiv.classList.remove("open");
-        mobileNavigationDiv.classList.remove("mobile-navigation--visible");
+        exitMenu();
+        mobileScroller.removeEventListener("click", exitMenu);
+        scrollerOverlay.removeEventListener("click", exitMenu);
     } else {
-        hamburgerDiv.dataset.status = "open";
-        hamburgerDiv.classList.add("open");
-        mobileNavigationDiv.classList.add("mobile-navigation--visible");
+        openMenu();
+        mobileScroller.addEventListener("click", exitMenu);
+        scrollerOverlay.addEventListener("click", exitMenu);
     }
 })
+
+const exitMenu = () => {
+    hamburgerDiv.dataset.status = "closed";
+    hamburgerDiv.classList.remove("open");
+    mobileNavigationDiv.classList.remove("mobile-navigation--visible");
+}
+
+const openMenu = () => {
+    hamburgerDiv.dataset.status = "open";
+    hamburgerDiv.classList.add("open");
+    mobileNavigationDiv.classList.add("mobile-navigation--visible");
+}
